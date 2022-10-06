858d2b15edb7cec78ef9e073319f7ec96919ace8
khilola.kushbakova <xilola_kushbakova@mai.ru>
2022-10-06 18:57:54 +0300

    change2

--- src/Part2.java
+++ src/Part2.java
@@ -1,19 +1,19 @@
 public class Part2 {
     public static void main(String[] args) {
 
-        System.out.println("Task Number 1");
+        System.out.println("Task Number 1\n");
 
         for (int i = 1904; i < 2096; i= i+4) {
             System.out.println(i + "   год является високосным");
         }
 
 
-        System.out.println("Task Number 2");
+        System.out.println("Task Number 2\n");
         for ( int i = 7; i <= 98; i = i+7) {
             System.out.println(i);
         }
 
-        System.out.println("Task Number 3");
+        System.out.println("Task Number 3\n");
 
 
         for (int i=1; i<=512; i= i*2) {
